c5965ede6ac0d205c40fe315963c1148d92d2863
Gregory Koberger <gkoberger@gmail.com>
2012-05-08 03:06:43 -0700

    Clean up styling a bit more

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if(($_GET['file'] == "manifest.webapp" || $_GET['file'] == "manifest.json") && $
 <div style="text-align: center;">
     <h1>Your random manifest is&hellip;</h1>
     <input type="text" id="thingy" value="<?= $manifest ?>"><br>
-    <div style="color: #999; font-size: 0.9em; padding-top: 3px;">
+    <div style="color: #999; font-size: 0.9em; padding-top: 10px;">
     Absolutely any subdomain will work &middot; <a href="/">get another</a>
     </div>
 </div>
@@ -36,6 +36,13 @@ if(($_GET['file'] == "manifest.webapp" || $_GET['file'] == "manifest.json") && $
         box-shadow: 0 3px #EEEEEE inset;
         padding: 8px;
         width: 500px;
+        text-align: center;
+    }
+    a, a:visited {
+        color: #000;
+    }
+    a:hover {
+        color: #555;
     }
 </style>
 <script>
