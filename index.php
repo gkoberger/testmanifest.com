449bf648b45777df24c429fd3252e9206ea91517
Gregory Koberger <gkoberger@gmail.com>
2012-05-08 16:14:31 -0700

    Move controls arould

--- index.php
+++ index.php
@@ -71,10 +71,12 @@ if($subdomain) {
 <div style="text-align: center;">
     <h1>Your random manifest is&hellip;</h1>
     <input type="text" id="thingy" value="<?= $manifest ?>">
+    <br>
     <a href="<?= $domain ?>">edit</a> &middot; <a href="#" id="install">install</a>
+    &middot; <a href="/">get another</a>
     <br>
     <div style="color: #999; font-size: 0.9em; padding-top: 10px;">
-    Absolutely any subdomain will work &middot; <a href="/">get another</a>
+    Absolutely any subdomain will work
     </div>
 </div>
 <script src="https://myapps.mozillalabs.com/jsapi/include.js"></script>
