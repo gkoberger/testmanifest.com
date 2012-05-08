512e5ec5d4f6fe67f38ab0ca197d19f7b8ccec84
Gregory Koberger <gkoberger@gmail.com>
2012-05-08 16:52:02 -0700

    Add a sidebar

--- template_edit.php
+++ template_edit.php
@@ -1,4 +1,15 @@
-<button id="install">Install!</button>
+<div id="sidebar">
+    <button id="install">Install!</button>
+    <hr>
+    If you update the manifest, it will be available at any of the following subdomains:
+    <ul>
+    <li>http://<?= $subdomain ?>-<span>test</span>.testmanifest.com</li>
+    <li>http://<?= $subdomain ?>-<span>1</span>.testmanifest.com</li>
+    <li>http://<?= $subdomain ?>-<span>2</span>.testmanifest.com</li>
+    <li>http://<?= $subdomain ?>-<span>new</span>.testmanifest.com</li>
+    <li>http://<?= $subdomain ?>-<span>&lt;anything&gt;</span>.testmanifest.com</li>
+    </ul>
+</div>
 
 <form method="POST">
     <button type='submit'>Save</button>
