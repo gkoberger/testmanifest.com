9ff2ad9d07fdc29a2e19955f15d9ca815841296b
Gregory Koberger <gkoberger@gmail.com>
2012-05-08 17:01:26 -0700

    Start styling stuff

--- header.php
+++ header.php
@@ -21,6 +21,83 @@
         a:hover {
             color: #555;
         }
+
+        body, html {
+             height: 100%;
+             margin: 0pt;
+             padding: 0pt;
+        }
+
+        #sidebar {
+             -moz-box-sizing: border-box;
+             -webkit-box-sizing: border-box;
+             background-color: #EEEEEE;
+             border-right: 1px solid #CCCCCC;
+             float: left;
+             height: 100%;
+             margin-right: 50px;
+             padding: 25px 50px 25px 25px;
+             width: 300px;
+        }
+
+        #sidebar ul {
+             color: #888888;
+             font-size: 0.7em;
+             line-height: 1.6em;
+             list-style: none outside none;
+             padding: 0pt 0pt 0pt 10px;
+        }
+
+        #sidebar hr {
+             background-color: #CCCCCC;
+             border-color: -moz-use-text-color -moz-use-text-color #FFFFFF;
+             border-style: none none solid;
+             border-width: 0pt 0pt 1px;
+             height: 2px;
+        }
+
+        #sidebar ul li span {
+             color: #555555;
+        }
+
+        #sidebar p {
+             color: #666666;
+             font-size: 0.9em;
+        }
+
+        form {
+            padding-top: 25px;
+            margin-left: 350px;
+        }
+        textarea {
+            height: 400px;
+            max-width: 700px;
+            width: 100%;
+            border: 1px solid #ccc;
+            padding: 1em;
+            -moz-box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+        }
+        #sidebar button:active {
+            position: relative;
+            top: 2px;
+        }
+        #sidebar button:hover {
+            background-color: #77ACD2;
+        }
+        #sidebar button {
+            cursor: pointer;
+            background-color: #88BBDE;
+            border: 0 none;
+            border-radius: 5px;
+            box-shadow: 0 -5px rgba(0, 0, 0, 0.1) inset;
+            color: #FFFFFF;
+            font-size: 1em;
+            font-weight: bold;
+            padding: 10px 10px 12px;
+            text-shadow: -1px -1px rgba(0, 0, 0, 0.5);
+            width: 100%;
+        }
     </style>
     <script src="https://myapps.mozillalabs.com/jsapi/include.js"></script>
   </head>
--- template_edit.php
+++ template_edit.php
@@ -1,20 +1,21 @@
 <div id="sidebar">
     <button id="install">Install!</button>
     <hr>
-    If you update the manifest, it will be available at any of the following subdomains:
+    <p>If you update the manifest, it will also be available at any of the following subdomains:</p>
     <ul>
-    <li>http://<?= $subdomain ?>-<span>test</span>.testmanifest.com</li>
-    <li>http://<?= $subdomain ?>-<span>1</span>.testmanifest.com</li>
-    <li>http://<?= $subdomain ?>-<span>2</span>.testmanifest.com</li>
-    <li>http://<?= $subdomain ?>-<span>new</span>.testmanifest.com</li>
-    <li>http://<?= $subdomain ?>-<span>&lt;anything&gt;</span>.testmanifest.com</li>
+    <li>http://<?= $subdomain_base ?>-<span>test</span>.testmanifest.com</li>
+    <li>http://<?= $subdomain_base ?>-<span>1</span>.testmanifest.com</li>
+    <li>http://<?= $subdomain_base ?>-<span>2</span>.testmanifest.com</li>
+    <li>http://<?= $subdomain_base ?>-<span>new</span>.testmanifest.com</li>
+    <li>http://<?= $subdomain_base ?>-<span>&lt;anything&gt;</span>.testmanifest.com</li>
     </ul>
+    <p>Subdomains are namespaced. Using a dash will allow you to serve manifests from different URLs while maintaining the manifest of the namespace.</p>
 </div>
 
 <form method="POST">
     <button type='submit'>Save</button>
 <br>
-    <textarea name='manifest' style='height: 400px; width: 600px; border: 1px solid #ccc;'><?= $json ?></textarea>
+    <textarea name='manifest'><?= $json ?></textarea>
     <br>
     <button type='submit'>Save</button>
 </form>
